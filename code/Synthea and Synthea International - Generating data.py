f4abc2a34a39d15512b47a3f577062086427c323
Sander Martins Gonçalves <sander.martinsgoncalves@gmail.com>
2020-11-19 23:57:23 +0100

    Updating seed and population info
    Updating seed used (12345) and population quantity (1k)

--- code/Synthea and Synthea International - Generating data.py
+++ code/Synthea and Synthea International - Generating data.py
@@ -4,7 +4,7 @@ The instructions here will be the same of the ones found at the page.
 """
 git clone https://github.com/synthetichealth/synthea.git #1 Clone the repository
 cd synthea #2 Go to the synthea folder
-./run_synthea #3 generate data
+./run_synthea #3 generate data. For this project we will be using [-s 12345 -p 1000]
 
 '''
 We can change the features of our data, like country and zip code. In order to do that, we need to use other repository as complementary data (https://github.com/synthetichealth/synthea-international). For this case, we choose the desired country from the available list (github page of synthea-international) and copy its contents into "synthea" folder. Afterwards is just running the code as usual from "synthea" folder (./run_synthea)
